7417d26dfb6be85bd83bcd2ff94bf87494d49f3f
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2025-07-17 21:33:15 +0300

    Initial Commit

---
+++ Inview/InviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InviewApp.swift
+//  Inview
+//
+//  Created by Sedef Bozkurt on 17.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct InviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
